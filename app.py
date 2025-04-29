af375f90d913ea08ff0f419ab4f86b6d7cd13889
臼田淳一 <usu@usutajunichinoMacBook-Air.local>
2025-04-29 20:53:31 +0900

    Add Crossref metadata endpoint

--- app.py
+++ app.py
@@ -1,35 +1,23 @@
-# app.py
-from fastapi import FastAPI, Query
 import requests
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
-@app.get("/search_papers")
-def search_papers(keyword: str = Query(..., description="検索するキーワード")):
-    url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
-    params = {
-        "db": "pubmed",
-        "term": keyword,
-        "retmode": "json",
-        "retmax": 5
+def get_crossref_metadata(doi):
+    url = f"https://api.crossref.org/works/{doi}"
+    response = requests.get(url)
+    if response.status_code != 200:
+        return {"error": f"DOI {doi} not found"}
+
+    data = response.json()["message"]
+    return {
+        "title": data.get("title", [""])[0],
+        "authors": [f"{a.get('given', '')} {a.get('family', '')}" for a in data.get("author", [])],
+        "journal": data.get("container-title", [""])[0],
+        "published": data.get("issued", {}).get("date-parts", [[""]])[0],
+        "abstract": data.get("abstract", "（抄録はありません）")
     }
-    response = requests.get(url, params=params)
-    data = response.json()
 
-    pmids = data.get('esearchresult', {}).get('idlist', [])
-    results = []
-    for pmid in pmids:
-        fetch_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
-        fetch_params = {
-            "db": "pubmed",
-            "id": pmid,
-            "retmode": "xml"
-        }
-        fetch_response = requests.get(fetch_url, params=fetch_params)
-        if fetch_response.status_code == 200:
-            results.append({
-                "pmid": pmid,
-                "link": f"https://pubmed.ncbi.nlm.nih.gov/{pmid}/"
-            })
-
-    return {"papers": results}
+@app.get("/get_metadata")
+def get_metadata(doi: str = Query(..., description="DOI (Digital Object Identifier)")):
+    return get_crossref_metadata(doi)
